4fbb21a272c01b3d1b5238c73c3a384650060d41
Alejandro <alejandro1996rd@gmail.com>
2026-04-30 18:38:42 +0200

    cambiado del game

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,5 +1,4 @@
 import Footer from "../components/footer"
-import Friends from "../components/friends"
 import Header from "../components/header"
 
 function Game() {
@@ -8,13 +7,10 @@ function Game() {
       <Header />
       <main className="app-content">
         <div className="grid content-grid">
-          <div className="col-12 lg:col-9">
+          <div className="col-12 lg:col-12">
             <section className="ui-surface game-main">
               <h1>Pagina de Game</h1>
             </section>
-          </div>
-          <div className="col-12 lg:col-3">
-            <Friends />
           </div>
         </div>
       </main>
