4f68b27e359bf2d8a9351e44807ad1c8c55e329d
neomikus <kiankocasaubon01@gmail.com>
2026-07-30 16:01:34 +0200

    Chat messages and friends tables created

--- backend/init.sql
+++ backend/init.sql
@@ -8,6 +8,9 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100),
     full_name VARCHAR(100),
+    user_role VARCHAR(6),
+    title VARCHAR(25) DEFAULT "Student",
+    bio VARCHAR(140) DEFAULT "I'm using Transcendence!",
     avatar_url TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -15,7 +18,7 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS posts (
     id SERIAL PRIMARY KEY,
-    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES users(id),
     author_username VARCHAR(50),
     content VARCHAR(240),
     likes INT[],
@@ -23,4 +26,20 @@ CREATE TABLE IF NOT EXISTS posts (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     parent INT DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS chat_messages (
+    id SERIAL PRIMARY KEY,
+    FOREIGN KEY (sender_id) REFERENCES users(id),
+    FOREIGN KEY (receiver_id) REFERENCES users(id),
+    content VARCHAR(240),
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS friends (
+    id SERIAL PRIMARY KEY,
+    FOREIGN KEY (sender_id) REFERENCES users(id),
+    FOREIGN KEY (receiver_id) REFERENCES users(id),
+    accepted BOOLEAN,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
