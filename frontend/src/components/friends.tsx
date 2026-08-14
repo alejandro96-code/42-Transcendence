13d55e14d40d0a9db6d1073ceba79b8b440a885b
andefern <anderferal@gmail.com>
2026-08-14 17:56:15 +0200

    funtional page

--- frontend/src/components/friends.tsx
+++ frontend/src/components/friends.tsx
@@ -5,7 +5,7 @@ import { InputText } from 'primereact/inputtext'
 import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog'
 import { Toast } from 'primereact/toast'
 import { friendsAPI, type Friend, type PendingFriendRequest } from '../services/friendsAPI'
-import { t } from 'i18next'
+import { useI18n } from '../hooks/useI18n'
 
 interface FriendsProps {
   selectedFriendId?: number | null
