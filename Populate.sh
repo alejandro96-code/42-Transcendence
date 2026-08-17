3e90db9bf43e8e704c8633980f31c2ba3ebf13ae
GoroXabi <xaberortega@gmail.com>
2026-08-17 17:19:13 +0200

    Curl to endpoint done with one user, vulnerability in the api behaivour detected and reported

--- Populate.sh
+++ Populate.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+NUMBER_OF_USERS="$1"
+
+if ! [[ "$NUMBER_OF_USERS" =~ ^[0-9]+$ ]]; then
+    echo "Usage error: insert a valid number of users"
+    exit 1
+fi
+
+curl -k -X POST https://10.14.8.6:8443/api/auth/register \
+  -H "Content-Type: application/json" \
+  -d '{
+    "username": "juan.perez",
+    "password": "Password123!",
+    "fullName": "Juan Pérez",
+    "email": "juan.perez@example.com"
+  }'
+
+echo "$NUMBER_OF_USERS"
